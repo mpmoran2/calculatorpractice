4c1ee8838ec95cbf362737098f2337c66c3b3c30
mpmoran2 <kamokumoran@gmail.com>
2021-04-28 14:39:49 -0700

    = Buttons work

--- calc.py
+++ calc.py
@@ -6,61 +6,84 @@ root.title('Calculator')
 
 # get user input and place in text field
 i = 0
+
+
 def get_numb(num):
     global i
     display.insert(i, num)
-    i+=1
+    i += 1
 
-# add function buttons
+# add arithmetic buttons
+def get_operation(operator):
+    global i
+    length = len(operator)
+    display.insert(i, operator)
+    i += length
+
+# add = button
+def calculate():
+    entire_string = display.get()
+    try:
+        a = parser.expr(entire_string).compile()
+        result = eval(a)
+        clear_all()
+        display.insert(0,result)
+    except Exception:
+        clear_all()
+        display.insert(0,"ERROR")
+
+# add deletion buttons
 def clear_all():
-    display.delete(0,END)
+    display.delete(0, END)
+
 
 def undo():
     entire_string = display.get()
     if len(entire_string):
         new_string = entire_string[:-1]
         clear_all()
-        display.insert(0,new_string)
+        display.insert(0, new_string)
     else:
         clear_all()
         display.insert(0, "ERROR")
+
 
 # add an input field
 display = Entry(root)
 display.grid(row=1, columnspan=6, sticky=W + E)
 
 # add number buttons
-Button(root, text="1", command = lambda :get_numb(1)).grid(row=2, column=0)
-Button(root, text="2", command = lambda :get_numb(2)).grid(row=2, column=1)
-Button(root, text="3", command = lambda :get_numb(3)).grid(row=2, column=2)
+Button(root, text="1", command=lambda: get_numb(1)).grid(row=2, column=0)
+Button(root, text="2", command=lambda: get_numb(2)).grid(row=2, column=1)
+Button(root, text="3", command=lambda: get_numb(3)).grid(row=2, column=2)
 
-Button(root, text="4", command = lambda :get_numb(4)).grid(row=3, column=0)
-Button(root, text="5", command = lambda :get_numb(5)).grid(row=3, column=1)
-Button(root, text="6", command = lambda :get_numb(6)).grid(row=3, column=2)
+Button(root, text="4", command=lambda: get_numb(4)).grid(row=3, column=0)
+Button(root, text="5", command=lambda: get_numb(5)).grid(row=3, column=1)
+Button(root, text="6", command=lambda: get_numb(6)).grid(row=3, column=2)
 
-Button(root, text="7", command = lambda :get_numb(7)).grid(row=4, column=0)
-Button(root, text="8", command = lambda :get_numb(8)).grid(row=4, column=1)
-Button(root, text="9", command = lambda :get_numb(9)).grid(row=4, column=2)
+Button(root, text="7", command=lambda: get_numb(7)).grid(row=4, column=0)
+Button(root, text="8", command=lambda: get_numb(8)).grid(row=4, column=1)
+Button(root, text="9", command=lambda: get_numb(9)).grid(row=4, column=2)
 
 # add other buttons
-Button(root, text="AC", command = lambda :clear_all()).grid(row=5, column=0)
-Button(root, text="0", command = lambda :get_numb(0)).grid(row=5, column=1)
-Button(root, text="=").grid(row=5, column=2)
+Button(root, text="AC", command=lambda: clear_all()).grid(row=5, column=0)
+Button(root, text="0", command=lambda: get_numb(0)).grid(row=5, column=1)
+Button(root, text="=", command=lambda: calculate()).grid(row=5, column=2)
 
-Button(root, text="+").grid(row=2, column=3)
-Button(root, text="-").grid(row=3, column=3)
-Button(root, text="*").grid(row=4, column=3)
-Button(root, text="/").grid(row=5, column=3)
+Button(root, text="+", command=lambda: get_operation("+")).grid(row=2, column=3)
+Button(root, text="-", command=lambda: get_operation("-")).grid(row=3, column=3)
+Button(root, text="*", command=lambda: get_operation("*")).grid(row=4, column=3)
+Button(root, text="/", command=lambda: get_operation("/")).grid(row=5, column=3)
 
 # adding functionalities
-Button(root, text="pi").grid(row=2, column=4)
-Button(root, text="%").grid(row=3, column=4)
-Button(root, text="(").grid(row=4, column=4)
-Button(root, text="exp").grid(row=5, column=4)
+Button(root, text="pi", command=lambda: get_operation("*3.14")).grid(row=2, column=4)
+Button(root, text="%", command=lambda: get_operation("%")).grid(row=3, column=4)
+Button(root, text="(", command=lambda: get_operation("(")).grid(row=4, column=4)
+Button(root, text="exp", command=lambda: get_operation("**")).grid(row=5, column=4)
 
-Button(root, text="<-", command = lambda :undo()).grid(row=2, column=5)
+Button(root, text="<-", command=lambda: undo()).grid(row=2, column=5)
 Button(root, text="x!").grid(row=3, column=5)
-Button(root, text=")").grid(row=4, column=5)
-Button(root, text="^2").grid(row=5, column=5)
+Button(root, text=")", command=lambda: get_operation(")")).grid(row=4, column=5)
+Button(root, text="^2", command=lambda: get_operation("**2")).grid(row=5, column=5)
 
 root.mainloop()
