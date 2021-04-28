b54aaa67d809460c878731499b7df4d9d3204c90
mpmoran2 <kamokumoran@gmail.com>
2021-04-28 14:24:29 -0700

    Clear and Clear All Buttons work

--- calc.py
+++ calc.py
@@ -11,6 +11,20 @@ def get_numb(num):
     display.insert(i, num)
     i+=1
 
+# add function buttons
+def clear_all():
+    display.delete(0,END)
+
+def undo():
+    entire_string = display.get()
+    if len(entire_string):
+        new_string = entire_string[:-1]
+        clear_all()
+        display.insert(0,new_string)
+    else:
+        clear_all()
+        display.insert(0, "ERROR")
+
 # add an input field
 display = Entry(root)
 display.grid(row=1, columnspan=6, sticky=W + E)
@@ -29,7 +43,7 @@ Button(root, text="8", command = lambda :get_numb(8)).grid(row=4, column=1)
 Button(root, text="9", command = lambda :get_numb(9)).grid(row=4, column=2)
 
 # add other buttons
-Button(root, text="AC").grid(row=5, column=0)
+Button(root, text="AC", command = lambda :clear_all()).grid(row=5, column=0)
 Button(root, text="0", command = lambda :get_numb(0)).grid(row=5, column=1)
 Button(root, text="=").grid(row=5, column=2)
 
@@ -44,7 +58,7 @@ Button(root, text="%").grid(row=3, column=4)
 Button(root, text="(").grid(row=4, column=4)
 Button(root, text="exp").grid(row=5, column=4)
 
-Button(root, text="<-").grid(row=2, column=5)
+Button(root, text="<-", command = lambda :undo()).grid(row=2, column=5)
 Button(root, text="x!").grid(row=3, column=5)
 Button(root, text=")").grid(row=4, column=5)
 Button(root, text="^2").grid(row=5, column=5)
